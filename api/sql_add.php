74efcdc7e52673e87756dcf0d9e47293f670cd6b
chyhhwen <chyhhwen@gmail.com>
2023-07-01 01:16:23 +0800

    test

--- api/sql_add.php
+++ api/sql_add.php
@@ -1,14 +1,40 @@
 <?php
 date_default_timezone_set('Asia/Taipei');
 $time = date('Y-m-d H:i:s');
-$dbname = "bookstore";
-$sql = "";
-$data = "";
-function conn($dbname)
+header('Content-type:application/json;charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+if (
+    empty($_POST['name']) ||
+    empty($_POST['data'])
+) {
+    $json = [
+        'ok' => false,
+        'message' => 'Please input all fields'
+    ];
+    $response = json_encode($json);
+    echo $response;
+    die();
+}
+// 若讀取失敗
+/*if (
+    empty($_POST['name'])
+) {
+    $json = array(
+        "ok" => false,
+        "message" => "Please input content"
+    );
+
+    $response = json_encode($json);
+    echo $response;
+    die();
+}*/
+
+/*function conn($dbname)
 {
     try
     {
-        $var = require "require.php";
+        $var = require "config.php";
+        $dbname = $var['dbname'];
         $pdo = new PDO("mysql:host=localhost;dbname=$dbname;charset=utf8",$var['user'],$var['pass']);
     }
     catch (PDOException $e)
@@ -17,13 +43,16 @@ function conn($dbname)
     }
     return $pdo;
 }
+$var = require "config.php";
+$dbname = $var['dbname'];
+
 $pdo = conn($dbname);
 $sth = $pdo->prepare($sql);
 try
 {
-    if ($sth->execute($data))
+    if ($sth->execute($var['field']))
     {
-        #ref([0,'error.php']);
+
     }
     else
     {
@@ -35,3 +64,9 @@ catch (PDOException $e)
     echo 'error';
 }
 unset($pdo);
+/*$json = array(
+    "comments" => $comments
+);
+$response = json_encode($json);
+header('Content-type：application/json;charset=utf-8');*/
+/*echo $response;*/
